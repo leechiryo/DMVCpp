2a97587973b95514a8cd2081cd5bd99a53defcff
智 李 <lizhi1977@hotmail.com>
2016-11-26 12:09:14 +0900

    Add some animations.

---
+++ MVCpp/mvc/ViewElements/AniButtonPressed.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Animation.h"
+
+namespace mvc {
+  class AniButtonPressed : Animation {
+
+  private:
+    ID2D1SolidColorBrush* m_pBrush;
+  protected:
+    virtual void CreateD2DResource() {
+      HRESULT hr = m_pRenderTarget->CreateSolidColorBrush(
+        D2D1::ColorF(0xcccccc),
+        &m_pBrush);
+
+      if (!SUCCEEDED(hr)) {
+        throw std::runtime_error("Failed to create the background brush.");
+      }
+
+    }
+
+    virtual void DestroyD2DResource() {
+      SafeRelease(m_pBrush);
+    }
+  public:
+    virtual bool DrawFrame(int frameIdx) {
+      // TODO:用10帧在点击的位置作出一个圆形，逐渐填充到整个区域。
+      return false;
+    }
+  };
+}
--- MVCpp/mvc/ViewElements/Animation.h
+++ MVCpp/mvc/ViewElements/Animation.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "..\Types.h"
 #include "..\View.h"
@@ -6,32 +6,30 @@
 
 namespace mvc {
 
-  enum class AniStatus{Stoped, Paused, Playing};
+  enum class AniStatus { Stoped, Paused, Playing };
+
+  typedef void(*AnimationCallBack)();
 
   class Animation : public View<Animation>
   {
   private:
-    int m_x;
-    int m_y;
     int m_frameIdx;
     int m_rotation;
     AniStatus m_status;
 
   public:
-    virtual void DrawFrame(int frameIdx) = 0;
-    virtual void OnFinished() { }
+
+    // 每一个动画对象都要实现此方法。
+    virtual bool DrawFrame(int frameIdx) = 0;
+
+    // 可以设置动画完成时的回调函数
+    AnimationCallBack OnFinished;
 
     Animation() {
-      m_x = 0;
-      m_y = 0;
       m_frameIdx = 0;
       m_rotation = 0;
       m_status = AniStatus::Stoped;
-    }
-
-    void SetPosition(int x, int y) {
-      m_x = x;
-      m_y = y;
+      OnFinished = nullptr;
     }
 
     void SetFrameIndex(int idx) {
@@ -42,19 +40,46 @@ namespace mvc {
       m_rotation = angle;
     }
 
-    void Show() {
-
-    }
-
     void Play() {
       m_status = AniStatus::Playing;
     }
 
     void Stop() {
+      m_frameIdx = 0;
       m_status = AniStatus::Stoped;
     }
+
     void Pause() {
       m_status = AniStatus::Paused;
+    }
+
+    // 绘制动画
+    virtual void DrawSelf() {
+
+      if (m_status == AniStatus::Playing) {
+
+        // 如果正处于播放状态，则在画面上绘制当前帧
+        if (DrawFrame(m_frameIdx)) {
+
+          // 如果当前帧完成后动画播放完毕(没有下一帧了)，则将下一帧设为0（动画开始的位置），
+          // 下次调用Show的时候将从头播放。
+          m_frameIdx = 0;
+
+          if (OnFinished) {
+            // 如果定义了播放完成后的回调函数，则调用它。
+            OnFinished();
+          }
+        }
+        else {
+
+          // 如果当前帧完成后动画并没有播放完毕，
+          // 则将当前帧的索引加1，下次调用Show时将绘制下一帧。
+          m_frameIdx++;
+        }
+      }
+      else if (m_status == AniStatus::Paused) {
+        DrawFrame(m_frameIdx);
+      }
     }
 
   };
