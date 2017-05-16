310f134f14a622604aeb6a48e503f2bdc71ec66e
智 李 <lizhi1977@hotmail.com>
2017-05-17 00:40:54 +0900

    add layout to checkbox.

--- MVCpp/mvc/ViewElements/CheckBox.h
+++ MVCpp/mvc/ViewElements/CheckBox.h
@@ -3,6 +3,9 @@
 #include "..\Types.h"
 #include "..\View.h"
 #include "..\ModelRef.h"
+#include "Text.h"
+#include "Rectangle.h"
+#include "Line.h"
 
 namespace mvc {
 
@@ -10,54 +13,58 @@ namespace mvc {
   {
   private:
 
-    // D2D 资源(离开作用域时会自动销毁)
-    DxResource<ID2D1SolidColorBrush> m_pBackgroundBrush;
-    DxResource<ID2D1SolidColorBrush> m_pBrush;
-    DxResource<IDWriteTextFormat> m_pTextFormat;
-
-    static const int MAX_CHARS = 256;
-    wchar_t m_font[MAX_CHARS + 1];
-    float m_fontSize;
-    DWRITE_FONT_WEIGHT m_fontWeight;
-    DWRITE_FONT_STYLE m_fontStyle;
-    DWRITE_FONT_STRETCH m_fontStretch;
-
-    UINT32 m_color;
+    shared_ptr<Text> m_title;
+    shared_ptr<Rectangle> m_rect;
+    shared_ptr<Line> m_line1;
+    shared_ptr<Line> m_line2;
 
     // controller method
     static LRESULT Handle_LBUTTONDOWN(shared_ptr<CheckBox> cbx, WPARAM wParam, LPARAM lParam) {
-      if (cbx->checked) cbx->checked = false;
-      else cbx->checked = true;
+      if (cbx->checked) {
+        cbx->checked = false;
+        cbx->m_line1->SetHidden(true);
+        cbx->m_line2->SetHidden(true);
+      }
+      else {
+        cbx->checked = true;
+        cbx->m_line1->SetHidden(false);
+        cbx->m_line2->SetHidden(false);
+      }
       return 0;
     }
 
   protected:
 
     virtual void CreateD2DResource() {
-
-      // 背景设为白色透明
-      m_pBackgroundBrush = m_pContext.CreateSolidColorBrush(D2D1::ColorF(0xffffff));
-      m_pBackgroundBrush->SetOpacity(0.0f);
-
-      m_pTextFormat = App::CreateTextFormat(m_font, m_fontSize, m_fontWeight, m_fontStyle, m_fontStretch);
-      m_pTextFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING);
-      m_pTextFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER);
-
-      m_pBrush = m_pContext.CreateSolidColorBrush(D2D1::ColorF(m_color));
     }
 
   public:
 
     ModelRef<bool> checked;
-    ModelRef<wstring> title;
 
-    CheckBox(const D2DContext &context, wstring ttl) : View(context), title{ ttl } {
-      m_color = 0x333333;
-      m_fontWeight = DWRITE_FONT_WEIGHT_REGULAR;
-      m_fontStyle = DWRITE_FONT_STYLE_NORMAL;
-      m_fontStretch = DWRITE_FONT_STRETCH_NORMAL;
-      wcscpy_s(m_font, MAX_CHARS + 1, L"Source Code Pro");
-      m_fontSize = 16.0;
+    CheckBox(const D2DContext &context, wstring ttl) : View(context) {
+
+      m_layout.AddRow("*");
+      m_layout.AddCol("*");
+
+      m_rect = AppendSubView<Rectangle>();
+      m_title = AppendSubView<Text>(ttl);
+      m_line1 = m_rect->AppendSubView<Line>();
+      m_line2 = m_rect->AppendSubView<Line>();
+
+      m_rect->AddLayoutRow("*");
+      m_rect->AddLayoutCol("*");
+      m_rect->SetStroke(2.0f);
+      m_rect->SetLeftOffset(0);
+      m_rect->SetWidth("10");
+      m_rect->SetHeight("10");
+      
+      m_line1->SetStroke(2.0f);
+      m_line2->SetStroke(2.0f);
+      m_line1->SetOffset(2, 5, 5.5, 2);
+      m_line2->SetOffset(4.5, 8, 2, 8);
+
+      m_title->SetLeftOffset(15);
 
       AddEventHandler(WM_LBUTTONDOWN, Handle_LBUTTONDOWN);
 
@@ -67,26 +74,16 @@ namespace mvc {
     ~CheckBox() {
     }
 
+    virtual float GetDefaultWidth() {
+      return m_title->GetDefaultWidth() + 15;
+    }
+
+    virtual float GetDefaultHeight() {
+      auto height = m_title->GetDefaultHeight();
+      return height > 10 ? height : 10;
+    }
+
     virtual void DrawSelf() {
-      D2D1_RECT_F textRect = RectD(m_left + 20, m_top, m_right, m_bottom);
-      m_pContext->FillRectangle(textRect, m_pBackgroundBrush.ptr());
-
-      double vstart = (m_bottom + m_top) / 2 - 5;
-      double vend = (m_bottom + m_top) / 2 + 5;
-
-      m_pContext->DrawRectangle(RectD(m_left, vstart, m_left + 10, vend), m_pBrush.ptr(), 2.0f);
-
-      if (checked) {
-        m_pContext->DrawLine(Point2D(m_left+2, vstart+5), Point2D(m_left+4.5, vend-2), m_pBrush.ptr(), 2.0f);
-        m_pContext->DrawLine(Point2D(m_left+4.5, vend-2), Point2D(m_left+8, vstart+2), m_pBrush.ptr(), 2.0f);
-      }
-
-      m_pContext->DrawText(
-        title->c_str(),
-        title->length(),
-        m_pTextFormat.ptr(),
-        textRect,
-        m_pBrush.ptr());
     }
 
   };
